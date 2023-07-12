f15254d5cfd36552aee7c5e76bf3697077cc51f9
BaboonIdeas <139361427+BaboonIdeas@users.noreply.github.com>
2023-07-12 12:35:43 -0300

    Site - OcasONG em breve

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import viteLogo from '/vite.svg'
+import baboonLogo from './assets/baboon-logo.png'
+import ocasongTemporario from './assets/ocasong-temporario.png'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a>
+          <img src={ocasongTemporario} className="logo" alt="Ocasong Temporario" />
+        </a>
+      </div>
+      <h1>OcasONG</h1>
+      <h2>Em breve um novo site...</h2>
+      {/* <p className="read-the-docs">
+        Em desenvenvolvimento por
+      </p>
+      <img src={baboonLogo} className="baboon react" alt="Baboon Ideas" /> */}
+    </>
+  )
+}
+
+export default App
